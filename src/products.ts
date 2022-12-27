a17f2c87ea2285f97f3882cbb5c0eea296d14b37
omerenma <godwin2341@gmail.com>
2022-12-27 02:50:11 +0100

    single spa server

---
+++ src/products.ts
@@ -0,0 +1,78 @@
+export interface Product {
+    id: number,
+    name: string,
+    price: number,
+    description: string,
+    image: string,
+    longDescription:string
+}
+
+const products: Product[] = [
+    {
+    id: 1,
+        name: "Wheel Spinner",
+        price: 5.99,
+        description: 'A wheel fidget spinner that spins wheels',
+        image: 'http://localhost:8080/dress.jpg',
+        longDescription:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    },
+      {
+    id: 2,
+        name: "White chair",
+        price: 6.99,
+        description: 'A wheel fidget spinner that spins wheels',
+        image: 'http://localhost:8080/bag.jpg',
+        longDescription:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    }
+    ,
+        {
+    id: 3,
+        name: "Wheel Spinner",
+        price: 5.99,
+        description: 'A wheel fidget spinner that spins wheels',
+        image: 'http://localhost:8080/clipper.jpg',
+        longDescription:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    },
+          {
+    id: 4,
+        name: "Wheel Spinner",
+        price: 5.99,
+        description: 'A wheel fidget spinner that spins wheels',
+        image: 'http://localhost:8080/oriamo.jpg',
+        longDescription:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    },
+            {
+    id: 5,
+        name: "Wheel Spinner",
+        price: 5.99,
+        description: 'A wheel fidget spinner that spins wheels',
+        image: 'http://localhost:8080/oraimo1.jpg',
+        longDescription:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    },
+              {
+    id: 6,
+        name: "Wheel Spinner",
+        price: 5.99,
+        description: 'A wheel fidget spinner that spins wheels',
+        image: 'http://localhost:8080/samsung.jpg',
+        longDescription:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    },
+                {
+    id: 7,
+        name: "Wheel Spinner",
+        price: 5.99,
+        description: 'A wheel fidget spinner that spins wheels',
+        image: 'http://localhost:8080/shoe.jpg',
+        longDescription:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    },
+                  {
+    id: 8,
+        name: "Wheel Spinner",
+        price: 5.99,
+        description: 'A wheel fidget spinner that spins wheels',
+        image: 'http://localhost:8080/uv.jpg',
+        longDescription:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    }
+]
+
+export default products
